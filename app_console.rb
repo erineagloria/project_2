261bd85821ffcac0ea28856928375f66f9b5b4c0
Erinea Gloria <erineagloria@gmail.com>
2020-07-23 21:35:47 +1000

    day two CRUD works

--- app_console.rb
+++ app_console.rb
@@ -4,11 +4,10 @@
 # eg: find_all_dishes()
 # and then pause with a repl
 
-require "pry"
 require "pg"
 require_relative "models/company.rb"
 require_relative "models/event.rb"
 
-binding.pry
+# binding.pry
 
 
--- main.rb
+++ main.rb
@@ -2,10 +2,9 @@
 require 'sinatra'
 
 require 'sinatra/reloader' if development? 
-also_reload "models/company" if development? 
-also_reload "models/event" if development? 
+# also_reload "models/company" if development? 
+# also_reload "models/event" if development? 
 
-require 'pry'
 require 'pg'
 
 # requiring our own files, relative from where we are
